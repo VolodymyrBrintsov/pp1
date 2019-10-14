6af1a0516e1462d2f59c4520a0b75f9d86956c3e
VolodymyrBrintsov <Brintsov@gmail.com>
2019-10-14 19:02:40 +0200

    first commit zd 6,7

---
+++ 01-TypesAndVariables/zadanie during class.py
@@ -0,0 +1,10 @@
+#zadanie 6
+x = [50, 'Janusz', True, 149.17, 4*7, 4.0*7, 2>5]
+for y in x:
+    print(type(y))
+    
+#zadanie 7
+x = [5 + 10*5, 3-2+1, 2+-3, 2**8, 4+4/2**2, 4%3%2%1, 1+2%3**4*5, True!=False, 2<=3 or False, not True or not False and not True,
+     2<3 and 4/5 and 6+7 < 8 or not 9+10 == 19, 0b11111 >> 1 >> 1 >> 1, 0x11 + 0b11 + 11, 2 << 3 >> 4]
+for y in x:
+    print(y)
