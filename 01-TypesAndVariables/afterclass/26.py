d6ee38cbe9312c52fdcf240b447e02829f1d438c
VolodymyrBrintsov <Brintsov@gmail.com>
2019-10-15 11:10:09 +0200

    Improved Version

--- 01-TypesAndVariables/afterclass/26.py
+++ 01-TypesAndVariables/afterclass/26.py
@@ -3,9 +3,12 @@ m = float(input())
 print('Podaj wzrost w metrach')
 h = float(input())
 I = round(m / h**2, 1)
-if I < 18.5:
-    print(f'Masz za malo wagi, twoj pokaznik BMI: {I}')
-elif 18.5 < I < 25:
-    print(f'Masz normalna wage, twoj pokaznik BMI: {I}')
-elif 24 < I:
-    print(f'Masz za duzo wagi, twoj pokaznik BMI: {I}')
+if h < 3.5:
+    if I < 18.5:
+        print(f'Masz za malo wagi, twoj pokaznik BMI: {I}')
+    elif 18.5 < I < 25:
+        print(f'Masz normalna wage, twoj pokaznik BMI: {I}')
+    elif 24 < I:
+        print(f'Masz za duzo wagi, twoj pokaznik BMI: {I}')
+elif h > 3.5:
+    print('Nie moge policzyc.Napisales wzrost w metrach')
