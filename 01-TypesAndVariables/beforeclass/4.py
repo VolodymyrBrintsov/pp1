ce49e01088b6da42466078fcdc69fe9dc350f79e
VolodymyrBrintsov <Brintsov@gmail.com>
2019-10-17 22:53:28 +0200

    BeforeClass

---
+++ 01-TypesAndVariables/beforeclass/4.py
@@ -0,0 +1,5 @@
+wiek = int(input('Napisz wiek: '))
+if wiek < 10:
+    print('Wiek jest mniejszy od 10')
+else:
+    print('Wiek jest wiekszy od 10')
---
+++ 01-TypesAndVariables/beforeclass/5.py
@@ -0,0 +1,5 @@
+wiek = int(input())
+if wiek < 18:
+    print('Osoba nie jest dorosla')
+else:
+    print('Osoba jest dorosla')
