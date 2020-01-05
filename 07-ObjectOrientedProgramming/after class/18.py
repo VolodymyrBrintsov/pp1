20aa1a504eddab948722e53b8388fd8e7843d126
VolodymyrBrintsov <Brintsov@gmail.com>
2020-01-05 03:48:11 +0100

    1

--- 07-ObjectOrientedProgramming/after class/18.py
+++ 07-ObjectOrientedProgramming/after class/18.py
@@ -6,9 +6,48 @@ class Pojazdy():
         self.nr_rejestracyjny = nr_rejestracyjny
         self.dostepnosc = "Wypozyczony"
         
+        
     def __str__(self):
         return f'Marka pojazdu: {self.marka}, rejestracyjny numer: {self.nr_rejestracyjny}, przebieg {self.przebieg} km, dostepnosc: {self.dostepnosc}'
-
+    
+    def zmianaPrzebiegu(self, przebieg):
+        self.przebieg = przebieg
+        
+    def zmianaStatusu_True(self):
+         self.dostepnosc = "Dostepny"
+        
+    def zmianaStatusu_False(self):
+        self.dostepnosc = "Wypozyczony"
+        
 p1 = Pojazdy('Roflianowa', 23134)
-print(p1)
+p2 = Pojazdy('Roflogleck',42124)
+p3 = Pojazdy('Francua', 85431)
+ListPojazow = [p1,p2,p3]
+
+class wypozyczalnie():
+    def __init__(self,ListPojazow,nazwa):
+        self.list = ListPojazow
+        self.nazwa = nazwa
+        
+    def wyswitl(self):
+        for e in self.list:
+            print(e)
+        
+    def dodacPojazd(self):
+        pass
+    
+w = wypozyczalnie(ListPojazow)
+w.wyswitl()
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
         
---
+++ 08-DataStructures/during class/9.py
@@ -0,0 +1,9 @@
+osoba = {
+    "imie": "Vova",
+    "nazwisko": "Brintsov",
+    "wiek": 16,
+    "hobby": ['programowanie'],
+    "student": True,
+    "telefon": {"stacjonarny": "2233", "komorkowy":"7788"}
+    }
+print()
